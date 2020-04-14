97f629f600219a820f75ff3c2f9b29194e9b575a
Maria Lagunero <maria58@gmail.com>
2020-04-14 16:31:05 -0700

    add changes

--- __tests__/sudoku.tests.js
+++ __tests__/sudoku.tests.js
@@ -1,8 +1,21 @@
 // sudoku_solver.js
 module.exports.parseBoard = function(board) {
 
-  describe('#parseBoard()', function () {
-    it('should parse a sudoku board into a 2D array', function () {
+  // sudoku_solver_spec.js
+  var board = 
+    '090000006\n' +
+    '000960485\n' +
+    '000581000\n' +
+    '004000000\n' +
+    '517200900\n' +
+    '602000370\n' +
+    '100804020\n' +
+    '706000810\n' +
+    '300090000';
+  var parsedBoard;
+
+  describe('#parseBoard()', function() {
+    it('should parse a sudoku board into a 2D array', function() {
       parsedBoard = parseBoard(board);
       var expectedBoard = [
         [0, 9, 0, 0, 0, 0, 0, 0, 6],
@@ -15,4 +28,10 @@ module.exports.parseBoard = function(board) {
         [7, 0, 6, 0, 0, 0, 8, 1, 0],
         [3, 0, 0, 0, 9, 0, 0, 0, 0]
       ];
+      expect(parsedBoard.length).to.equal(9);
+      expect(parsedBoard[0].length).to.equal(9);
+      expect(parsedBoard).to.eql(expectedBoard);
+    });
+  });
+
 };
